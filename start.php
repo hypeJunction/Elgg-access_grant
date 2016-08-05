bdb715b8125d356c2b66e58d2eea30f2108d2fea
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-08-05 16:19:59 +0200

    feat(solr): now integrated with elgg_solr

--- start.php
+++ start.php
@@ -16,7 +16,12 @@ elgg_register_event_handler('init', 'system', 'access_grant_init');
  */
 function access_grant_init() {
 
-	elgg_register_plugin_hook_handler('get_sql', 'access', 'access_grant_get_sql');
+	if (elgg_is_active_plugin('elgg_solr')) {
+		elgg_register_plugin_hook_handler('elgg_solr:index', 'all', 'access_grant_solr_index');
+		elgg_register_plugin_hook_handler('elgg_solr:access', 'all', 'access_grant_solr_access_query');
+	} else {
+		elgg_register_plugin_hook_handler('get_sql', 'access', 'access_grant_get_sql');
+	}
 }
 
 /**
@@ -54,5 +59,65 @@ function access_grant_get_sql($hook, $type, $return, $params) {
 						AND er_access_grant.relationship = 'access_grant'
 						AND er_access_grant.guid_two = {$user_guid}))";
 
+	return $return;
+}
+
+use Solarium\QueryType\Update\Query\Document\DocumentInterface;
+
+/**
+ * Index access grants
+ *
+ * @param string            $hook   "elgg_solr:index"
+ * @param string            $type   "object"|"group"|"user"
+ * @param DocumentInterface $return Solr document
+ * @param array             $params Hook params
+ * @return DocumentInterface
+ */
+function access_grant_solr_index($hook, $type, $return, $params) {
+
+	if (!$return) {
+		return;
+	}
+
+	$entity = elgg_extract('entity', $params);
+	if (!elgg_instanceof($entity)) {
+		return;
+	}
+
+	$access_grants = [];
+	$access_grants_batch = new ElggBatch('elgg_get_entities_from_relationship', [
+		'type' => 'user',
+		'relationship' => 'access_grant',
+		'relationship_guid' => $entity->guid,
+		'limit' => 0,
+		'callback' => false,
+	]);
+
+	foreach ($access_grants_batch as $user) {
+		$access_grants[] = $user->guid;
+	}
+
+	$return->access_grants_is = $access_grants;
+	
+	return $return;
+}
+
+/**
+ * Update solr access query to include access grants
+ * 
+ * @param string $hook   "elgg_solr:access"
+ * @param string $type   "entities"
+ * @param array  $return Queries
+ * @param array  $params Hook params
+ * @return array
+ */
+function access_grant_solr_access_query($hook, $type, $return, $params) {
+
+	$user_guid = elgg_extract('user_guid', $params);
+	if (!$user_guid) {
+		return;
+	}
+	
+	$return['ors']['access_granted'] = "access_grants_is:($user_guid)";
 	return $return;
 }
